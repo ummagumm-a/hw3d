8db6764514734bda7f9b6042aed70f39cb2757b9
ummagumm_a <siniy.vyacheslav@gmail.com>
2021-01-10 21:02:16 +0300

    First commit

---
+++ WinMain.cpp
@@ -0,0 +1,42 @@
+﻿#include "Window.h"
+
+int CALLBACK WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR	  lpCmdLine,
+	int		  nCmdShow)
+{
+	try
+	{
+		Window wnd(800, 300, "Donkey Fart Box");
+
+		// message pump
+		MSG msg;
+		BOOL gResult;
+		while ((gResult = GetMessageA(&msg, nullptr, 0, 0)) > 0)
+		{
+			TranslateMessage(&msg);
+			DispatchMessageA(&msg);
+		}
+
+		if (gResult == -1)
+		{
+			return -1;
+		}
+
+		return msg.wParam;
+	}
+	catch (const ChiliException& e)
+	{
+		MessageBoxA(nullptr, e.what(), e.GetType(), MB_OK | MB_ICONEXCLAMATION);
+	}
+	catch (const std::exception& e)
+	{
+		MessageBoxA(nullptr, e.what(), "Standard Exception", MB_OK | MB_ICONEXCLAMATION);
+	}
+	catch (...)
+	{
+		MessageBoxA(nullptr, "No details available", "Unknown Exception", MB_OK | MB_ICONEXCLAMATION);
+	}
+	return -1;
+}
