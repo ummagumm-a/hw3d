4bb95e6afcffe28be47716f93141f7052ca79c7e
ummagumm_a <siniy.vyacheslav@gmail.com>
2021-02-04 22:44:33 +0300

    nice architecture, fascinating little cubes

--- App.cpp
+++ App.cpp
@@ -1,9 +1,27 @@
 #include "App.h"
-#include <sstream>
+#include "Box.h"
+#include <memory>
 
 App::App()
 	:
 	wnd(800, 600, "The Donkey Fart Box")
+{
+	std::mt19937 rng(std::random_device{}());
+	std::uniform_real_distribution<float> adist(0.0f, 3.1415f * 2.0f);
+	std::uniform_real_distribution<float> ddist(0.0f, 3.1415f * 2.0f);
+	std::uniform_real_distribution<float> odist(0.0f, 3.1415f * 0.3f);
+	std::uniform_real_distribution<float> rdist(-20.0f, 20.0f);
+	for (auto i = 0; i < 200; i++)
+	{
+		boxes.push_back(std::make_unique<Box>(
+			wnd.Gfx(), rng, adist,
+			ddist, odist, rdist
+			));
+	}
+	wnd.Gfx().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 3.0f / 4.0f, 0.5f, 50.0f));
+}
+
+App::~App()
 {}
 
 int App::Go()
@@ -22,19 +40,13 @@ int App::Go()
 
 void App::DoFrame()
 {
-	const float c = sin(timer.Peek()) / 2.0f + 0.5f;
-
-	wnd.Gfx().ClearBuffer(c, 0.6, 0.1);
-
-	wnd.Gfx().DrawTestTriangle(
-		timer.Peek(),
-		0.0f, 0.0f
-	);
-	wnd.Gfx().DrawTestTriangle(
-		timer.Peek(),
-		wnd.mouse.GetPosX() / 400.0f - 1.0f,
-		-wnd.mouse.GetPosY() / 300.0f + 1.0f
-	);
+	auto dt = timer.Mark();
+	wnd.Gfx().ClearBuffer(0.07f, 0.0f, 0.12f);
+	for (auto& b : boxes)
+	{
+		b->Update(0.1f);
+		b->Draw(wnd.Gfx());
+	}
 	
 	wnd.Gfx().EndFrame();
 }
