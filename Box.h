6252397e1477f2988c02b74946baf57f4f6408c2
ummagumm_a <siniy.vyacheslav@gmail.com>
2021-02-05 20:09:23 +0300

    static bindables (crashes, but try 1 box only)

--- Box.h
+++ Box.h
@@ -1,7 +1,7 @@
 #pragma once
-#include "Drawable.h"
+#include "DrawableBase.h"
 
-class Box : public Drawable
+class Box : public DrawableBase<Box>
 {
 public:
 	Box(Graphics& gfx, std::mt19937& rng,
@@ -27,6 +27,4 @@ private:
 	float dtheta;
 	float dphi;
 	float dchi;
-
-	bool trigger = true;
 };
